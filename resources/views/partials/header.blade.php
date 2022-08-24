791f597bdf3ff54ae3d7c82cfe46d0fb61fcfe4f
NicolaVerlato <nicola.verlato00@gmail.com>
2022-08-24 16:19:34 +0200

    header done

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,45 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="DC Logo">
+        </div>
+        <div class="header-list">
+            <ul>
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+                <li class="underline">
+                    <a href="#">COMICS</a>
+                </li>
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+                <li>
+                    <a href="#">TV</a>
+                </li>
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="#">FANS</a>
+                </li>
+                <li>
+                    <a href="#">NEWS</a>
+                </li>
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="search">
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+</header>
