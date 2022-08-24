062cb8da2d84f8efc53e73dbc8e2cbe42ba3336e
NicolaVerlato <nicola.verlato00@gmail.com>
2022-08-24 19:47:36 +0200

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,4 +19,41 @@
             @endforeach
         </div>
     </section>
+
+    <section class="merch">
+       <div class="container">
+            <ul>
+                <li>
+                    <img src="{{asset('images/buy-comics-digital-comics.png') }}" alt="dc">
+                    <div>
+                        DIGITAL COMICS
+                    </div>
+                </li>
+                <li>
+                    <img src="{{asset('images/buy-comics-merchandise.png') }}" alt="merch">
+                    <div>
+                        DC MERCHANDISE
+                    </div>
+                </li>
+                <li>
+                    <img src="{{asset('images/buy-comics-subscriptions.png') }}" alt="sub">
+                    <div>
+                        SUBSCRIPTION
+                    </div>
+                </li>
+                <li>
+                    <img src="{{asset('images/buy-comics-shop-locator.png') }}" alt="shop">
+                    <div>
+                        COMIC SHOP LOCATOR
+                    </div>
+                </li>
+                <li>
+                    <img src="{{asset('images/buy-dc-power-visa.svg') }}" alt="visa">
+                    <div>
+                        DC POWER VISA
+                    </div>
+                </li>
+            </ul>
+       </div>
+    </section>
 @endsection
